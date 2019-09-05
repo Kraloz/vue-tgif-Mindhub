f0f0e724e28f6fde4858b005839a427e0ebc5831
Tomás Aprile <tomas.eaprile@gmail.com>
2019-09-05 09:40:19 -0300

    acomodando weas

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
 import router from './router'
 import store from './store'
+//styles
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
