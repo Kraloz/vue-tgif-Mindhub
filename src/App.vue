00f893fe8a95344f0389042014398c9310d85231
Tomás Aprile <tomas.eaprile@gmail.com>
2019-09-05 09:39:52 -0300

    "agregados estilos"

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,27 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <Header/>
+    <main>
+      <router-view/>
+    </main>
+    <Footer/>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Footer from '@/components/Footer.vue'
+export default {
+  name: 'App',
+  components: {
+    Header: Header,
+    Footer: Footer
+  }
 }
+</script>
+  
+<style>
+  @import './assets/css/bg.css';
+  @import './assets/css/styles.css';
 </style>
