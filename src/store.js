df9f009e7fe5ebe46333fbe4e31bcc4510d8e723
Tomás Aprile <tomas.aprile@fnetsystem.com.ar>
2019-09-08 15:31:44 -0300

    added env vars

--- src/store.js
+++ src/store.js
@@ -6,9 +6,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     loading: false,
-    url: '',
+    url: 'https://api.propublica.org/congress/v1/113/house/members.json',
     members: [],
-    key: process.env.VUE_APP_API_KEY
+    key: process.env.VUE_APP_SECRET,
+    stats: {}
   },
   mutations: {
     SET_LOADING_STATUS(state, status) {
@@ -19,11 +20,11 @@ export default new Vuex.Store({
     }
   },
   actions: {
-    fetchMembers(context){
+    fetchMembers(context) {
       context.commit('SET_LOADING_STATUS', true)
-      fetch(this.url, {
+      fetch(this.state.url, {
         method: 'GET',
-        // mode: 'cors',
+        mode: 'cors',
         headers: {
           'X-API-Key': this.state.key
         }
@@ -33,13 +34,19 @@ export default new Vuex.Store({
         }
         return response.json()
       }).then(json => {
-        context.commit('SET_MEMBERS', json.results[0].members)
         context.commit('SET_LOADING_STATUS', false)
+        context.commit('SET_MEMBERS', json.results[0].members)
       }).catch(error => {
         context.commit('SET_LOADING_STATUS', false)
         // eslint-disable-next-line no-console
         console.error(error)
       })
     }
+  },
+  getters: {
+    membersLength: state => {
+      return state.members.length
+    }
   }
+
 })
