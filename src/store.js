f0c948614dd27dec103d80efcb918974281c97c7
Tomás Aprile <tomas.aprile@fnetsystem.com.ar>
2019-09-10 17:25:49 -0300

    WIP store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import Party from '@/assets/js/Party.js'
 
 Vue.use(Vuex)
 
@@ -9,7 +10,11 @@ export default new Vuex.Store({
     url: 'https://api.propublica.org/congress/v1/113/house/members.json',
     members: [],
     key: process.env.VUE_APP_SECRET,
-    stats: {}
+    stats: {
+        d: new Party('Democrats'),
+        r: new Party('Republicans'),
+        i: new Party('Independents')
+    }
   },
   mutations: {
     SET_LOADING_STATUS(state, status) {
@@ -17,6 +22,11 @@ export default new Vuex.Store({
     },
     SET_MEMBERS(state, members) {
       state.members = members
+    },
+    SET_MEMBERS_PARTY(state, party, members) {
+      console.log('existe: ', party, members)
+      // TODO: revisar acá broski - members pasa como undefined wtf ¿?
+      // state.stats[party].members = members
     }
   },
   actions: {
@@ -36,11 +46,42 @@ export default new Vuex.Store({
       }).then(json => {
         context.commit('SET_LOADING_STATUS', false)
         context.commit('SET_MEMBERS', json.results[0].members)
+      
+        context.dispatch('fillParties')
       }).catch(error => {
         context.commit('SET_LOADING_STATUS', false)
         // eslint-disable-next-line no-console
         console.error(error)
       })
+    },
+    fillParties(context) {
+      let list = {
+        d: [],
+        r: [],
+        i: []
+      }
+      this.state.members.forEach(member => {
+        switch (member.party.toLowerCase()) {
+          case 'd':
+            list.d.push(member)
+            break
+          case 'r':
+            list.r.push(member)
+            break
+          case 'i':
+            list.i.push(member)
+            break
+        }
+      })
+      // for (const party in list) {
+      //   // eslint-disable-next-line no-prototype-builtins
+      //   if (list.hasOwnProperty(party)) {
+      //     context.commit('SET_MEMBERS_PARTY', party, list[party])
+      //   }
+      // }
+      console.log(list.d)
+      // TODO: revisar acá broski
+      context.commit('SET_MEMBERS_PARTY', 'd', [...list.d])
     }
   },
   getters: {
